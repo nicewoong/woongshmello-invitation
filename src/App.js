6764e14ebf647b680d9ec693bac049dde413acd5
nicewoong <thoughtscode@gmail.com>
2023-07-29 23:21:11 +0900

    delte farebase codde

--- src/App.js
+++ src/App.js
@@ -4,16 +4,16 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import "photoswipe/dist/photoswipe.css";
 import ScrollToTop from "./components/ScrollToTop";
-import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA1K_xkWQM9UqxL8YEPVAgF5TYraDjxwMY",
-  authDomain: "woongshmello.firebaseapp.com",
-  projectId: "woongshmello",
-  storageBucket: "woongshmello.appspot.com",
-  messagingSenderId: "97751048916",
-  appId: "1:97751048916:web:f8207c5d4a5d9f2c4ad141"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA1K_xkWQM9UqxL8YEPVAgF5TYraDjxwMY",
+//   authDomain: "woongshmello.firebaseapp.com",
+//   projectId: "woongshmello",
+//   storageBucket: "woongshmello.appspot.com",
+//   messagingSenderId: "97751048916",
+//   appId: "1:97751048916:web:f8207c5d4a5d9f2c4ad141"
+// };
 
 
 const App = () => {
@@ -21,7 +21,7 @@ const App = () => {
   useEffect(() => {
     AOS.init();
     document.body.classList.add("loaded");
-    const app = initializeApp(firebaseConfig);
+    // const app = initializeApp(firebaseConfig);
   }, []);
 
   return (
