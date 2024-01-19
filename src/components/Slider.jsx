31d30d320422da87e5366502984fb3661ddfdb38
nicewoong <thoughtscode@gmail.com>
2024-01-20 02:00:21 +0900

    modify photo

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -8,7 +8,7 @@ const Slider = () => {
                 <div className="background">
                     <div
                         className="image"
-                        style={{ backgroundImage: "url(img/slider/1-3.jpg)" }}
+                        style={{ backgroundImage: "url(img/slider/1-3.png)" }}
                     ></div>
                 </div>
                 {/* End .background */}
